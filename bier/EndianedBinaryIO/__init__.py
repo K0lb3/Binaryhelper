571826653b9a436a7a9a9581c80a6143ad20be0b
Rudolf Kolbe <rkolbe96@gmail.com>
2025-04-29 21:29:33 +0200

    feat(EndianedBinaryIO) - initial version without arrays

---
+++ bier/EndianedBinaryIO/__init__.py
@@ -0,0 +1,10 @@
+from .EndianedBufferedReader import EndianedBufferedReader as EndianedBufferedReader
+from .EndianedBufferedWriter import EndianedBufferedWriter as EndianedBufferedWriter
+from .EndianedBytesIO import EndianedBytesIO as EndianedBytesIO
+from .EndianedBytesIO import Endianess as Endianess
+from .EndianedFileIO import EndianedFileIO as EndianedFileIO
+from .EndianedIOBase import (
+    EndianedIOBase as EndianedIOBase,
+    EndianedReaderIOBase as EndianedReaderIOBase,
+    EndianedWriterIOBase as EndianedWriterIOBase,
+)
