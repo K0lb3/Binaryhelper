156d16c75255ea6c7d2f87059bd85edc2daef2e7
Rudolf Kolbe <rkolbe96@gmail.com>
2025-05-12 16:07:05 +0200

    fix(setup): use zig compiler for unified interface, fix license, set min python to 3.13

--- setup.py
+++ setup.py
@@ -1,53 +1,36 @@
-import os
+from setuptools import Extension, find_packages, setup
+from st_zig import enforce_via_build_ext
 
-from setuptools import Extension, setup
-from setuptools.command.build_ext import build_ext
-
-INSTALL_DIR = os.path.dirname(os.path.realpath(__file__))
-UNITYPYBOOST_DIR = os.path.join(INSTALL_DIR, "UnityPyBoost")
-
-
-class BuildExt(build_ext):
-    def build_extensions(self):
-        compiler = self.compiler
-        # msvc - only ever used c++20, never c++2a
-        if compiler.compiler_type == "msvc":
-            cpp_version_flag = "/std:c++23preview"
-        # gnu & clang
-        else:
-            cpp_version_flag = "-std=c++23"
-
-        for ext in self.extensions:
-            ext.extra_compile_args = [cpp_version_flag]
-
-        build_ext.build_extensions(self)
+enforce_via_build_ext()
 
 
 setup(
     name="bier",
-    package_dir={"": "bier"},
+    packages=find_packages(),
     ext_modules=[
         Extension(
-            "EndianedBinaryIO._EndianedBytesIO",
+            "bier.EndianedBinaryIO._EndianedBytesIO",
             ["src/EndianedBinaryIO/EndianedBytesIO.cpp"],
             depends=["src/EndianedBinaryIO/PyConverter.hpp"],
             language="c++",
             include_dirs=["src"],
+            extra_compile_args=["-std=c++23"],
         ),
         Extension(
-            "EndianedBinaryIO._EndianedStreamIO",
+            "bier.EndianedBinaryIO._EndianedStreamIO",
             ["src/EndianedBinaryIO/EndianedStreamIO.cpp"],
             depends=["src/EndianedBinaryIO/PyConverter.hpp"],
             language="c++",
             include_dirs=["src"],
+            extra_compile_args=["-std=c++23"],
         ),
         Extension(
-            "EndianedBinaryIO._EndianedIOBase",
+            "bier.EndianedBinaryIO._EndianedIOBase",
             ["src/EndianedBinaryIO/EndianedIOBase.cpp"],
             depends=["src/PyConverter.hpp"],
             language="c++",
             include_dirs=["src"],
+            extra_compile_args=["-std=c++23"],
         ),
     ],
-    cmdclass={"build_ext": BuildExt},
 )
