674022b3d5bf395dbfc071c3a37e5209e6d843b6
LukeFZ <17146677+LukeFZ@users.noreply.github.com>
2025-07-08 23:55:38 +0200

    experiment with options rework, add member based length provider

--- bier/serialization/builtin_aliases.py
+++ bier/serialization/builtin_aliases.py
@@ -1,23 +1,14 @@
-from .options import length_type, custom
+from .options import custom, length_provider
 
 # useful aliases
 
-type list_d[TType, LType: length_type] = custom[list[TType], LType]
-type str_d[T: length_type] = custom[str, T]
-type bytes_d[T: length_type] = custom[bytes, T]
-
-# option aliases
-default_length_encoding = length_type
-"""
-Used for specifying the default type to use when reading a length-providing field.
-Must be serializing an 'int' type.
-"""
+type list_d[TType, LType: length_provider] = custom[list[TType], LType]
+type str_d[T: length_provider] = custom[str, T]
+type bytes_d[T: length_provider] = custom[bytes, T]
 
 __all__ = (
     # type aliases
     "list_d",
     "str_d",
     "bytes_d",
-    # option aliases
-    "default_length_encoding",
 )
