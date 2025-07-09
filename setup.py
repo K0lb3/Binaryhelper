2ee2120a753a1845edf8c7e917b485db666e1ed6
Rudolf Kolbe <rkolbe96@gmail.com>
2025-07-10 00:28:26 +0200

    fix(setup.py): install st_zig for windows

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 import sys
+import platform
 from setuptools import Extension, find_packages, setup
-from st_zig import enforce_via_build_ext
 
-enforce_via_build_ext()
+if platform.system() == "Windows":
+    # msvc compiler does not support C++23 yet, so have to use zig
+    # currently zig runs into some issues on setuptools on the gh workers
+    from st_zig import enforce_via_build_ext
+
+    enforce_via_build_ext()
 
 # only use the limited API if Python 3.11 or newer is used
 # 3.11 added PyBuffer support to the limited API,
@@ -40,4 +45,5 @@ setup(
         #     extra_compile_args=["-std=c++23"],
         # ),
     ],
+    install_requires=['st_zig; platform_system=="Windows"'],
 )
