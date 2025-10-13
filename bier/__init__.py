3d95a593b8fcccbdbab17b5b1a38249b3daed64b
Rudolf Kolbe <rkolbe96@gmail.com>
2025-10-13 15:50:42 +0200

    fix: only import serialization on python >= 3.13

--- bier/__init__.py
+++ bier/__init__.py
@@ -1,4 +1,10 @@
+from sys import version_info
+
 from . import EndianedBinaryIO as EndianedBinaryIO
-from . import serialization as serialization
+
+if version_info >= (3, 13):
+    from . import serialization as serialization
 
 __version__ = "0.0.1"
+
+__all__ = ["EndianedBinaryIO", "serialization", "__version__"]
