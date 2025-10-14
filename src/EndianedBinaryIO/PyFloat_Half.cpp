6b0f2c3af7799f332be8236d0b95853898b8a0c9
Rudolf Kolbe <rkolbe96@gmail.com>
2025-10-14 17:10:07 +0200

    fix(PyFloat - C): use version dependent type, format

--- src/EndianedBinaryIO/PyFloat_Half.cpp
+++ src/EndianedBinaryIO/PyFloat_Half.cpp
@@ -58,138 +58,170 @@ static_assert(sizeof(half) == 2, "half must be 2 bytes");
 static_assert(PY_MINOR_VERSION >= 12, "Python version must be at least 3.12 to use limited API");
 // 3.12 adds Py_INFINITY
 
-double PyFloat_Unpack2(const half &data) noexcept {
-  const auto &p = data.bytes[1];
-  const bool sign = ((p >> 7) & 1) != 0;
-  const uint32_t frac = (static_cast<uint32_t>(p & 0x03) << 8) |
-                        static_cast<uint32_t>(data.bytes[0]);
-  int32_t exp = static_cast<int32_t>((p & 0x7C) >> 2);
-  double x{};
+double PyFloat_Unpack2(const half &data) noexcept
+{
+    const auto &p = data.bytes[1];
+    const bool sign = ((p >> 7) & 1) != 0;
+    const uint32_t frac = (static_cast<uint32_t>(p & 0x03) << 8) |
+                          static_cast<uint32_t>(data.bytes[0]);
+    int32_t exp = static_cast<int32_t>((p & 0x7C) >> 2);
+    double x{};
 
-  if (exp == 0x1f) {
-    if (frac == 0) {
-      /* Infinity */
-      return sign ? -Py_INFINITY : Py_INFINITY;
-    } else {
-      /* NaN */
-      uint64_t v = sign ? 0xfff0000000000000ULL : 0x7ff0000000000000ULL;
+    if (exp == 0x1f)
+    {
+        if (frac == 0)
+        {
+            /* Infinity */
+            return sign ? -Py_INFINITY : Py_INFINITY;
+        }
+        else
+        {
+            /* NaN */
+            uint64_t v = sign ? 0xfff0000000000000ULL : 0x7ff0000000000000ULL;
 
-      v += static_cast<uint64_t>(frac) << 42; /* add NaN's type & payload */
-      return std::bit_cast<double>(v);
+            v += static_cast<uint64_t>(frac) << 42; /* add NaN's type & payload */
+            return std::bit_cast<double>(v);
+        }
     }
-  }
 
-  x = static_cast<double>(frac) / 1024.0;
+    x = static_cast<double>(frac) / 1024.0;
 
-  if (exp == 0) {
-    exp = -14;
-  } else {
-    x += 1.0;
-    exp -= 15;
-  }
-  x = std::ldexp(x, exp);
+    if (exp == 0)
+    {
+        exp = -14;
+    }
+    else
+    {
+        x += 1.0;
+        exp -= 15;
+    }
+    x = std::ldexp(x, exp);
 
-  if (sign) {
-    x = -x;
-  }
+    if (sign)
+    {
+        x = -x;
+    }
 
-  return x;
+    return x;
 }
 
-int PyFloat_Pack2(double x, half &data) noexcept {
-  bool sign{};
-  int32_t exp{};
-  double frac{};
-  uint16_t bits{};
+int PyFloat_Pack2(double x, half &data) noexcept
+{
+    bool sign{};
+    int32_t exp{};
+    double frac{};
+    uint16_t bits{};
 
-  if (x == 0.0) {
-    sign = (std::copysign(1.0, x) == -1.0);
-    exp = 0;
-    bits = 0;
-  } else if (std::isinf(x)) {
-    sign = (x < 0.0);
-    exp = 0x1f;
-    bits = 0;
-  } else if (std::isnan(x)) {
-    sign = (std::copysign(1.0, x) == -1.0);
-    exp = 0x1f;
-    uint64_t v = std::bit_cast<uint64_t>(x);
-    v &= 0xffc0000000000ULL;
-    bits = static_cast<uint16_t>(v >> 42); /* NaN's type & payload */
-  } else {
-    sign = (x < 0.0);
-    if (sign) {
-      x = -x;
-    }
-
-    frac = std::frexp(x, &exp);
-    if (frac < 0.5 || frac >= 1.0) {
-      PyErr_SetString(PyExc_SystemError, "frexp() result out of range");
-      return -1;
-    }
-
-    /* Normalize frac to be in the range [1.0, 2.0) */
-    frac *= 2.0;
-    exp--;
-
-    if (exp >= 16) {
-      goto Overflow;
-    } else if (exp < -25) {
-      /* |x| < 2**-25. Underflow to zero. */
-      frac = 0.0;
-      exp = 0;
-    } else if (exp < -14) {
-      /* |x| < 2**-14. Gradual underflow */
-      frac = std::ldexp(frac, 14 + exp);
-      exp = 0;
-    } else /* if (!(exp == 0 && frac == 0.0)) */ {
-      exp += 15;
-      frac -= 1.0; /* Get rid of leading 1 */
-    }
-
-    frac *= 1024.0; /* 2**10 */
-    /* Round to even */
-    bits = static_cast<uint16_t>(frac); /* Note the truncation */
-    assert(bits < 1024);
-    assert(exp < 31);
-    if ((frac - bits > 0.5) || ((frac - bits == 0.5) && (bits % 2 == 1))) {
-      ++bits;
-      if (bits == 1024) {
-        /* The carry propagated out of a string of 10 1 bits. */
+    if (x == 0.0)
+    {
+        sign = (std::copysign(1.0, x) == -1.0);
+        exp = 0;
         bits = 0;
-        ++exp;
-        if (exp == 31)
-          goto Overflow;
-      }
     }
-  }
+    else if (std::isinf(x))
+    {
+        sign = (x < 0.0);
+        exp = 0x1f;
+        bits = 0;
+    }
+    else if (std::isnan(x))
+    {
+        sign = (std::copysign(1.0, x) == -1.0);
+        exp = 0x1f;
+        uint64_t v = std::bit_cast<uint64_t>(x);
+        v &= 0xffc0000000000ULL;
+        bits = static_cast<uint16_t>(v >> 42); /* NaN's type & payload */
+    }
+    else
+    {
+        sign = (x < 0.0);
+        if (sign)
+        {
+            x = -x;
+        }
 
-  bits |= (exp << 10) | (sign << 15);
+        frac = std::frexp(x, &exp);
+        if (frac < 0.5 || frac >= 1.0)
+        {
+            PyErr_SetString(PyExc_SystemError, "frexp() result out of range");
+            return -1;
+        }
 
-  /* Write out result. */
-  /* First byte */
-  data.bytes[1] = static_cast<uint8_t>((bits >> 8) & 0xFF);
-  /* Second byte */
-  data.bytes[0] = static_cast<uint8_t>(bits & 0xFF);
+        /* Normalize frac to be in the range [1.0, 2.0) */
+        frac *= 2.0;
+        exp--;
 
-  return 0;
+        if (exp >= 16)
+        {
+            goto Overflow;
+        }
+        else if (exp < -25)
+        {
+            /* |x| < 2**-25. Underflow to zero. */
+            frac = 0.0;
+            exp = 0;
+        }
+        else if (exp < -14)
+        {
+            /* |x| < 2**-14. Gradual underflow */
+            frac = std::ldexp(frac, 14 + exp);
+            exp = 0;
+        }
+        else /* if (!(exp == 0 && frac == 0.0)) */
+        {
+            exp += 15;
+            frac -= 1.0; /* Get rid of leading 1 */
+        }
+
+        frac *= 1024.0; /* 2**10 */
+        /* Round to even */
+        bits = static_cast<uint16_t>(frac); /* Note the truncation */
+        assert(bits < 1024);
+        assert(exp < 31);
+        if ((frac - bits > 0.5) || ((frac - bits == 0.5) && (bits % 2 == 1)))
+        {
+            ++bits;
+            if (bits == 1024)
+            {
+                /* The carry propagated out of a string of 10 1 bits. */
+                bits = 0;
+                ++exp;
+                if (exp == 31)
+                    goto Overflow;
+            }
+        }
+    }
+
+    bits |= (exp << 10) | (sign << 15);
+
+    /* Write out result. */
+    /* First byte */
+    data.bytes[1] = static_cast<uint8_t>((bits >> 8) & 0xFF);
+    /* Second byte */
+    data.bytes[0] = static_cast<uint8_t>(bits & 0xFF);
+
+    return 0;
 
 Overflow:
-  PyErr_SetString(PyExc_OverflowError, "float too large to pack with e format");
-  return -1;
+    PyErr_SetString(PyExc_OverflowError, "float too large to pack with e format");
+    return -1;
 }
 #else
 #include "floatobject.h"
-#if PY_MINOR_VERSION < 11
-#define PyFloat_Pack2 _PyFloat_Pack2
-#define PyFloat_Unpack2 _PyFloat_Unpack2
-#endif
 double PyFloat_Unpack2(const half &data) noexcept
 {
+#if PY_MINOR_VERSION < 11
+    return _PyFloat_Unpack2(reinterpret_cast<const unsigned char *>(&data), 1);
+#else
     return PyFloat_Unpack2(reinterpret_cast<const char *>(&data), 1);
+#endif
 }
 int PyFloat_Pack2(double x, half &data) noexcept
 {
+#if PY_MINOR_VERSION < 11
+    return _PyFloat_Pack2(x, reinterpret_cast<unsigned char *>(&data), 1);
+#else
     return PyFloat_Pack2(x, reinterpret_cast<char *>(&data), 1);
+#endif
 }
 #endif
