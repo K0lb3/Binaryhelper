55588599fd7459b434e0c982d3964802959e450f
Rudolf Kolbe <rkolbe96@gmail.com>
2025-10-14 16:36:38 +0200

    feat(build): build wheels for 3.8-3.10 and 3.11 abi3

--- setup.py
+++ setup.py
@@ -2,15 +2,13 @@ import platform
 import sys
 
 from setuptools import Extension, find_packages, setup
+from setuptools.command.bdist_wheel import bdist_wheel
 
 if platform.system() == "Windows":
     extra_compile_args = ["/std:c++latest"]
 else:
     extra_compile_args = ["-std=c++23"]
 
-# only use the limited API if Python 3.11 or newer is used
-# 3.11 added PyBuffer support to the limited API,
-py_limited_api = sys.version_info >= (3, 11)
 default_sources = [
     "src/EndianedBinaryIO/PyFloat_Half.cpp",
 ]
@@ -19,6 +17,23 @@ default_depends = [
     "src/EndianedBinaryIO/PyConverter.hpp",
     "src/EndianedBinaryIO/PyFloat_Half.hpp",
 ]
+
+
+class bdist_wheel_abi3(bdist_wheel):
+    def get_tag(self):
+        python, abi, plat = super().get_tag()
+
+        if python.startswith("cp"):
+            # on CPython, our wheels are abi3 and compatible back to 3.6
+            return python, "abi3", plat
+
+        return python, abi, plat
+
+
+# only use the limited API if Python 3.11 or newer is used
+# 3.11 added PyBuffer support to the limited API,
+py_limited_api = sys.version_info >= (3, 11)
+cmdclass = {"bdist_wheel": bdist_wheel_abi3} if py_limited_api else {}
 
 setup(
     name="bier",
@@ -52,4 +67,5 @@ setup(
         #     extra_compile_args=["-std=c++23"],
         # ),
     ],
+    cmdclass=cmdclass,
 )
